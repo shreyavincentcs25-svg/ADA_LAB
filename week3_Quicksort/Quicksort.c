bf4d547f20c4f048a0426f22526f08de6f51d8d9
shreyavincentcs25-svg <shreyavincent.cs25@bmsce.ac.in>
2026-04-24 12:46:20 +0530

    Add files via upload

---
+++ week3_Quicksort/Quicksort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int partition(int a[], int low, int high) {
+    int pivot = a[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+
+    do {
+        while (i <= high && a[i] <= pivot)
+            i++;
+
+        while (a[j] > pivot)
+            j--;
+
+        if (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+    } while (i < j);
+
+    // swap pivot with a[j]
+    temp = a[low];
+    a[low] = a[j];
+    a[j] = temp;
+
+    return j;
+}
+
+void quickSort(int a[], int low, int high) {
+    if (low < high) {
+        int p = partition(a, low, high);
+        quickSort(a, low, p - 1);
+        quickSort(a, p + 1, high);
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    quickSort(a, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
